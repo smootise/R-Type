c7c5376cf8e63c1d89b15d5514268cb3af0d04cf
Vroenguard <corentin.bajdek@gmail.com>
2014-12-27 15:41:54 +0100

    Ajout des librairies d'IA sur linux
    Les test sont en cours (..)

--- R-Type(Corentin)/R-Type Server/LinDllLoader.cpp
+++ R-Type(Corentin)/R-Type Server/LinDllLoader.cpp
@@ -8,4 +8,58 @@ LinDllLoader::LinDllLoader()
 
 LinDllLoader::~LinDllLoader()
 {
+	//on free les librairies
+	for (it_type it = _availlable_libs.begin(); it != _availlable_libs.end(); it++)
+	{
+		dlclose(it->second);
+		std::cout << "Librairie closed : " << it->first << std::endl;
+	}
+}
+
+void				LinDllLoader::loadfromfile(const std::string &filename)
+{
+	std::string		line;
+	std::ifstream	myfile(filename);
+
+	if (myfile.is_open())
+	{
+		while (std::getline(myfile, line))
+		if (line.empty() == false) //si la ligne est valide
+		{
+			if (_availlable_libs.count(line) <= 0) //si la lib n'est pas déja présente ds la map
+			{
+				void				*lib = NULL;
+
+				lib = dlopen(line.append(".so").c_str(), RTD_LAZY); //on load la librairie
+				if (lib != NULL)
+				{
+					_availlable_libs[line] = lib;
+					std::cout << "j'ajoute une lib : " << line.append(".so") << std::endl;
+				}
+				else
+					std::cerr << "unable to load lib : " << line.append(".so") << std::endl;
+			}
+		}
+		myfile.close();
+	}
+	else
+		std::cerr << "unable to open the file that contains libs to load" << std::endl;
+}
+
+//le nom envoyé doit correspondre a celui qui est dans le fichier a la lettre près !
+AMonster	*LinDllLoader::get_instance(const std::string &type, int timing)
+{
+	AMonster	*(*external_creator)(int);
+
+	if (_availlable_libs.count(type) != NULL)
+	{
+		external_creator = reinterpret_cast<AMonster* (*)(int)>(dlsym(_availlable_libs[type], "create"));
+		AMonster	*ret;
+
+		ret = external_creator(timing);
+		return (ret);
+	}
+	else
+		std::cerr << "unable to get instance of lib :" << type << std::endl;
+	return (NULL);
 }
