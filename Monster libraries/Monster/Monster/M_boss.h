4d250fb20c5b1f7b3a4b060e86d758bc5efb69c3
Vroenguard <corentin.bajdek@gmail.com>
2014-12-28 11:42:54 +0100

    Librairies deleted :/
    Test

--- Monster libraries/Monster/Monster/M_boss.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Monster.h"
-class M_boss :
-	public Monster
-{
-public:
-	M_boss(int time);
-	~M_boss();
-};
--- Monster libraries/Monster/Monster/M_light.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Monster.h"
-class M_light :
-	public Monster
-{
-public:
-	M_light(int time);
-	~M_light();
-};
