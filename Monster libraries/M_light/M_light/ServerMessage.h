c7a64637a7b24c383d8cfbe3e4bfaf45ae7648d2
Vroenguard <corentin.bajdek@gmail.com>
2014-12-28 12:43:40 +0100

    Update libs
    update libs

--- Monster libraries/M_light/M_light/ServerMessage.h
+++ Monster libraries/M_light/M_light/ServerMessage.h
@@ -1,6 +1,39 @@
 #pragma once
 
-enum
+//enum monster_type :
+// ==> AMonster.h
+
+enum shots_type
+{
+	ALLY,
+	ENEMI,
+};
+
+//this one is used for monsters and shots
+enum info
+{
+	TYPE,
+	ID,
+	POS_X,
+	POS_Y,
+};
+
+//this one is for monsters
+enum monsters
+{
+	DIRECTION = 4,
+	HP,
+};
+
+enum shots
+{
+	DIR_X = 4,
+	DIR_Y,
+};
+
+
+//player
+enum players
 {
 	J1,
 	J2,
@@ -15,8 +48,15 @@ struct ServerMessage
 	double	posx[4];
 	double	posy[4];
 	int		direction[4];
+	double	current_cd[4];
 
-	//additional data 
-	char	add_info[4096];
+	//monsters data
+	int		monsters[30][6];
+
+	//shots data
+	int		shots[100][6];
+
+	//used client side only
 	bool	has_been_read;
+	bool	is_game_over;
 };
